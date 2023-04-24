e32591f6543bc4e692bdab2bcf7bdb3224b8bb6c
Zermarden <zermarden@gmail.com>
2023-04-24 18:13:50 +0600

    Fix linters' errors

--- script/addBooks.js
+++ script/addBooks.js
@@ -7,14 +7,13 @@ const bookList = document.getElementById('books-list');
 // Get the existing data from localStorage, or create an empty array
 let storedData = JSON.parse(localStorage.getItem('storedData')) || [];
 
-// Create an object with the form data
 let authorData = [{
   bookName: 'Mark Twain',
   bookAuthor: 'The Adventures of Tom Sawyer',
   bookNumber: 123456,
 }];
 
-let clear= document.getElementById('clear');
+const clear = document.getElementById('clear');
 
 function newAuthorData() {
   authorData = {
@@ -31,8 +30,31 @@ function deleteBook(bookNumber) {
   localStorage.setItem('storedData', JSON.stringify(storedData));
 }
 
+function showBookList(authorData) {
+  const bookNameRow = document.createElement('tr');
+  const bookAuthorRow = document.createElement('tr');
+  const deleteRow = document.createElement('tr');
+  const newBookName = document.createElement('td');
+  const newBookAuthor = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  newBookName.innerText = authorData.bookName;
+  newBookAuthor.innerText = authorData.bookAuthor;
+  deleteButton.innerHTML = 'Delete';
+  bookNameRow.append(newBookName);
+  bookAuthorRow.append(newBookAuthor);
+  deleteRow.append(newBookName, newBookAuthor, deleteButton);
+  deleteRow.classList.add('book');
+  bookList.append(deleteRow);
+  deleteButton.addEventListener('click', () => {
+    deleteButton.parentElement.remove();
+    deleteBook(authorData.bookNumber);
+  });
+}
+
+storedData.forEach(showBookList);
+
 // Listen for form submission
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault(); // prevent form from submitting
   if (bookName.value !== '' && bookAuthor.value !== '') {
     newAuthorData();
@@ -40,35 +62,12 @@ form.addEventListener('submit', function (event) {
     form.reset();
 } else {
   // Optional: display a success message
-  alert('Data saved successfully!');
+  alert('Enter valid values for title and author fields, please.');
   }
 });
 
-clear.addEventListener('click',(e) => {
+clear.addEventListener('click', (e) => {
     e.preventDefault();
     localStorage.clear();
     alert('localstorage cleared')
-})
-
-function showBookList(authorData) {
-    const bookNameRow = document.createElement('tr');
-    const bookAuthorRow = document.createElement('tr');
-    const deleteRow = document.createElement('tr');
-    const newBookName = document.createElement('td');
-    const newBookAuthor = document.createElement('td');
-    const deleteButton = document.createElement('button');
-    newBookName.innerText = authorData.bookName;
-    newBookAuthor.innerText = authorData.bookAuthor;
-    deleteButton.innerHTML = 'Delete';
-    bookNameRow.append(newBookName);
-    bookAuthorRow.append(newBookAuthor);
-    deleteRow.append(newBookName, newBookAuthor, deleteButton);
-    deleteRow.classList.add("book");
-    bookList.append(deleteRow);
-    deleteButton.addEventListener('click', () => {
-      deleteButton.parentElement.remove();
-      deleteBook(authorData.bookNumber);
-    });
-}
-
-storedData.forEach(showBookList);
+});
